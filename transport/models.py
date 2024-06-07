cc2aeb265c539130b5062ae9cb7dee563b0f94b7
Jørgen H. Fjeld <jorgen@veridit.no>
2024-06-06 16:52:27 -1000

    db: Generate models from database
    With:
    `docker-compose run web python manage.py inspect_transport_db`
    
    I have tried several round of using AI to to improve on the
    inspect command, but they all fail, and I think it is too time
    consuming for me to do it.
    I'll just go with this generated model, and modify from here by hand
    (or with AI).
    
    The AI is pretty good at modifying concrete code, but to modify Meta
    code, i.e. code that generates/modifies code, seems to be outside of
    it's current ability.

--- transport/models.py
+++ transport/models.py
@@ -1,27 +1,26 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
-from django.contrib.auth.models import User
+
 
 class AbandonTrips(models.Model):
     id = models.BigAutoField(primary_key=True)
     abandon_date = models.DateTimeField(blank=True, null=True)
     notes = models.TextField()
-    reservation = models.ForeignKey('Reservations', on_delete=models.DO_NOTHING)
-    user = models.ForeignKey(User, on_delete=models.DO_NOTHING, blank=True, null=True)
+    reservation = models.ForeignKey('Reservations', models.DO_NOTHING)
+    user = models.ForeignKey('AuthUser', models.DO_NOTHING, blank=True, null=True)
     mile_charges = models.FloatField()
     calculate_fine = models.BooleanField()
     miles = models.SmallIntegerField()
 
     class Meta:
+        managed = False
         db_table = 'transport_abandon_trips'
-
-
-class DriverLinksPosition(models.Model):
-    id = models.IntegerField(primary_key=True)
-    position = models.IntegerField()
-    driver_login = models.BooleanField()
-
-    class Meta:
-        db_table = 'transport_driver_links_position'
 
 
 class GlobalSettings(models.Model):
@@ -29,6 +28,7 @@ class GlobalSettings(models.Model):
     leader_code = models.CharField(max_length=20)
 
     class Meta:
+        managed = False
         db_table = 'transport_global_settings'
 
 
@@ -41,54 +41,16 @@ class InfoLinks(models.Model):
     display_flag = models.CharField(max_length=1, blank=True, null=True)
 
     class Meta:
+        managed = False
         db_table = 'transport_info_links'
-
-
-class Log(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-    login_datetime = models.DateTimeField(blank=True, null=True)
-    logout_datetime = models.DateTimeField(blank=True, null=True)
-    ip_address = models.CharField(max_length=16)
-
-    class Meta:
-        db_table = 'transport_log'
-
-
-class Reservations(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    vehicle = models.ForeignKey('Vehicles', on_delete=models.DO_NOTHING)
-    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-    planned_passenger_no = models.CharField(max_length=2)
-    coordinator_approval = models.CharField(max_length=15)
-    planned_departure_datetime = models.DateTimeField(blank=True, null=True)
-    planned_return_datetime = models.DateTimeField(blank=True, null=True)
-    overnight = models.BooleanField()
-    child_seat = models.BooleanField()
-    destination = models.CharField(max_length=100)
-    reservation_cancelled = models.BooleanField()
-    reg_date = models.DateTimeField()
-    cancelled_by_driver = models.BooleanField()
-    driver_cancelled_time = models.DateTimeField(blank=True, null=True)
-    key_no = models.CharField(max_length=4, blank=True, null=True)
-    card_no = models.CharField(max_length=8, blank=True, null=True)
-    billing_department = models.ForeignKey('Departments', on_delete=models.DO_NOTHING, db_column='billing_department')
-    assigned_driver = models.ForeignKey('Driver', on_delete=models.DO_NOTHING, db_column='assigned_driver', related_name='assigned_reservations')
-    repeating = models.BooleanField()
-    deleted_by_driver = models.ForeignKey('Driver', on_delete=models.DO_NOTHING, db_column='deleted_by_driver', blank=True, null=True, related_name='deleted_reservations')
-    deleted_datetime = models.DateTimeField(blank=True, null=True)
-    no_cost = models.BooleanField()
-
-    class Meta:
-        db_table = 'transport_reservations'
 
 
 class RestrictedCharges(models.Model):
     id = models.BigAutoField(primary_key=True)
-    vehicle = models.ForeignKey('Vehicles', on_delete=models.DO_NOTHING)
+    vehicle = models.ForeignKey('Vehicles', models.DO_NOTHING)
     charge_month = models.CharField(max_length=2)
     charge_year = models.CharField(max_length=4)
-    department = models.ForeignKey('Departments', on_delete=models.DO_NOTHING)
+    department = models.ForeignKey('Departments', models.DO_NOTHING)
     calculation_method = models.CharField(max_length=50)
     total_charge = models.FloatField()
     begin_mileage = models.CharField(max_length=7, blank=True, null=True)
@@ -97,50 +59,21 @@ class RestrictedCharges(models.Model):
     reg_date = models.DateTimeField(blank=True, null=True)
 
     class Meta:
+        managed = False
         db_table = 'transport_restricted_charges'
-
-
-class ShopTasks(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-    vehicle = models.ForeignKey('Vehicles', on_delete=models.DO_NOTHING)
-    mileage_reading = models.CharField(max_length=7)
-    last_mileage = models.CharField(max_length=7)
-    work_type = models.ForeignKey('WorkType', on_delete=models.DO_NOTHING)
-    work_start_date = models.DateField(blank=True, null=True)
-    next_oil_change = models.DateField(blank=True, null=True)
-    total_cost = models.FloatField()
-    parts_source = models.CharField(max_length=255)
-    drive_test_done = models.BooleanField()
-    task_complete = models.BooleanField()
-    technician_comments = models.TextField()
-    reg_date = models.DateTimeField()
-    invoice_no = models.CharField(max_length=50)
-    vendor_name = models.CharField(max_length=50)
-
-    class Meta:
-        db_table = 'transport_shop_tasks'
-
-
-class TempMassEmails(models.Model):
-    id = models.CharField(max_length=250, primary_key=True)
-    driver_name = models.CharField(max_length=50)
-
-    class Meta:
-        db_table = 'transport_temp_mass_emails'
 
 
 class Vehicles(models.Model):
     id = models.BigAutoField(primary_key=True)
-    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    user = models.ForeignKey('AuthUser', models.DO_NOTHING)
     vehicle_no = models.CharField(max_length=12)
     vin_no = models.CharField(max_length=20)
     oil_filter = models.CharField(max_length=20)
     safety_date = models.DateField(blank=True, null=True)
     registration_date = models.DateField(blank=True, null=True)
     license_plate_no = models.CharField(max_length=50)
-    make = models.ForeignKey('VehicleBrand', on_delete=models.DO_NOTHING)
-    model = models.ForeignKey('VehicleType', on_delete=models.DO_NOTHING)
+    make = models.ForeignKey('VehicleBrand', models.DO_NOTHING)
+    model = models.ForeignKey('VehicleType', models.DO_NOTHING)
     manufacture_year = models.CharField(max_length=4)
     mileage_at_takeover = models.CharField(max_length=7)
     date_at_takeover = models.DateField(blank=True, null=True)
@@ -158,32 +91,33 @@ class Vehicles(models.Model):
     admin_issues = models.TextField()
 
     class Meta:
+        managed = False
         db_table = 'transport_vehicles'
 
 
-class WorkType(models.Model):
-    type = models.CharField(max_length=255)
+class InfoLinksPosition(models.Model):
+    link = models.ForeignKey(InfoLinks, models.DO_NOTHING)
+    position = models.IntegerField()
+    driver_login = models.BooleanField()
+    id = models.IntegerField(primary_key=True)
 
     class Meta:
-        db_table = 'transport_work_type'
+        managed = False
+        db_table = 'transport_info_links_position'
 
 
-class VehicleLimit(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    option = models.SmallIntegerField()
-    department = models.ForeignKey('Departments', on_delete=models.DO_NOTHING)
-    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-    from_date = models.DateField(blank=True, null=True)
-    to_date = models.DateField(blank=True, null=True)
-    limit_value = models.SmallIntegerField()
+class TempMassEmails(models.Model):
+    email = models.CharField(max_length=250)
+    driver_name = models.CharField(max_length=50)
 
     class Meta:
-        db_table = 'transport_vehicle_limit'
+        managed = False
+        db_table = 'transport_temp_mass_emails'
 
 
 class Driver(models.Model):
-    user = models.OneToOneField(User, on_delete=models.DO_NOTHING)
-    department = models.ForeignKey('Departments', on_delete=models.DO_NOTHING)
+    user = models.OneToOneField('AuthUser', models.DO_NOTHING)
+    department = models.ForeignKey('Departments', models.DO_NOTHING)
     phone = models.CharField(max_length=25)
     birth_date = models.DateField(blank=True, null=True)
     license_no = models.CharField(max_length=20)
@@ -207,6 +141,7 @@ class Driver(models.Model):
     max_passengers = models.BigIntegerField()
 
     class Meta:
+        managed = False
         db_table = 'transport_driver'
 
 
@@ -223,54 +158,145 @@ class Departments(models.Model):
     info = models.CharField(max_length=200)
 
     class Meta:
+        managed = False
         db_table = 'transport_departments'
+
+
+class VehicleLimit(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    option = models.SmallIntegerField()
+    department = models.ForeignKey(Departments, models.DO_NOTHING)
+    user = models.ForeignKey('AuthUser', models.DO_NOTHING)
+    from_date = models.DateField(blank=True, null=True)
+    to_date = models.DateField(blank=True, null=True)
+    limit_value = models.SmallIntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'transport_vehicle_limit'
+
+
+class Reservations(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    vehicle = models.ForeignKey(Vehicles, models.DO_NOTHING)
+    user = models.ForeignKey('AuthUser', models.DO_NOTHING)
+    planned_passenger_no = models.CharField(max_length=2)
+    coordinator_approval = models.CharField(max_length=15)
+    planned_departure_datetime = models.DateTimeField(blank=True, null=True)
+    planned_return_datetime = models.DateTimeField(blank=True, null=True)
+    overnight = models.BooleanField()
+    child_seat = models.BooleanField()
+    destination = models.CharField(max_length=100)
+    reservation_cancelled = models.BooleanField()
+    reg_date = models.DateTimeField()
+    cancelled_by_driver = models.BooleanField()
+    driver_cancelled_time = models.DateTimeField(blank=True, null=True)
+    key_no = models.CharField(max_length=4, blank=True, null=True)
+    card_no = models.CharField(max_length=8, blank=True, null=True)
+    billing_department = models.ForeignKey(Departments, models.DO_NOTHING, db_column='billing_department')
+    assigned_driver = models.ForeignKey(Driver, models.DO_NOTHING, db_column='assigned_driver')
+    repeating = models.BooleanField()
+    deleted_by_driver = models.ForeignKey(Driver, models.DO_NOTHING, db_column='deleted_by_driver', blank=True, null=True)
+    deleted_datetime = models.DateTimeField(blank=True, null=True)
+    no_cost = models.BooleanField()
+
+    class Meta:
+        managed = False
+        db_table = 'transport_reservations'
 
 
 class CommentLog(models.Model):
     id = models.BigAutoField(primary_key=True)
-    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    user = models.ForeignKey('AuthUser', models.DO_NOTHING)
     comments = models.TextField()
     comment_time = models.DateTimeField()
 
     class Meta:
+        managed = False
         db_table = 'transport_comment_log'
+
+
+class Log(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey('AuthUser', models.DO_NOTHING)
+    login_datetime = models.DateTimeField(blank=True, null=True)
+    logout_datetime = models.DateTimeField(blank=True, null=True)
+    ip_address = models.CharField(max_length=16)
+
+    class Meta:
+        managed = False
+        db_table = 'transport_log'
+
+
+class ShopTasks(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey('AuthUser', models.DO_NOTHING)
+    vehicle = models.ForeignKey(Vehicles, models.DO_NOTHING)
+    mileage_reading = models.CharField(max_length=7)
+    last_mileage = models.CharField(max_length=7)
+    work_type = models.ForeignKey('WorkType', models.DO_NOTHING)
+    work_start_date = models.DateField(blank=True, null=True)
+    next_oil_change = models.DateField(blank=True, null=True)
+    total_cost = models.FloatField()
+    parts_source = models.CharField(max_length=255)
+    drive_test_done = models.BooleanField()
+    task_complete = models.BooleanField()
+    technician_comments = models.TextField()
+    reg_date = models.DateTimeField()
+    invoice_no = models.CharField(max_length=50)
+    vendor_name = models.CharField(max_length=50)
+
+    class Meta:
+        managed = False
+        db_table = 'transport_shop_tasks'
+
+
+class WorkType(models.Model):
+    type = models.CharField(max_length=255)
+
+    class Meta:
+        managed = False
+        db_table = 'transport_work_type'
 
 
 class SpecialNotice(models.Model):
     id = models.BigAutoField(primary_key=True)
     notice_date = models.DateTimeField(blank=True, null=True)
-    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    user = models.ForeignKey('AuthUser', models.DO_NOTHING)
     title = models.CharField(max_length=255)
     notice = models.TextField()
 
     class Meta:
+        managed = False
         db_table = 'transport_special_notice'
 
 
 class ServiceReservations(models.Model):
     id = models.BigAutoField(primary_key=True)
     reg_date = models.DateTimeField()
-    vehicle = models.ForeignKey('Vehicles', on_delete=models.DO_NOTHING)
+    vehicle = models.ForeignKey(Vehicles, models.DO_NOTHING)
     from_datetime = models.DateTimeField(blank=True, null=True)
     to_datetime = models.DateTimeField(blank=True, null=True)
     is_cancelled = models.BooleanField()
     service_type = models.CharField(max_length=15)
 
     class Meta:
+        managed = False
         db_table = 'transport_service_reservations'
 
 
 class ServiceReservationsDetails(models.Model):
-    service_reservation = models.ForeignKey(ServiceReservations, on_delete=models.DO_NOTHING)
-    reservation = models.ForeignKey(Reservations, on_delete=models.DO_NOTHING)
+    service_reservation = models.ForeignKey(ServiceReservations, models.DO_NOTHING)
+    reservation = models.ForeignKey(Reservations, models.DO_NOTHING)
 
     class Meta:
+        managed = False
         db_table = 'transport_service_reservations_details'
 
 
 class TripDetails(models.Model):
     id = models.BigAutoField(primary_key=True)
-    reservation = models.ForeignKey(Reservations, on_delete=models.DO_NOTHING)
+    reservation = models.ForeignKey(Reservations, models.DO_NOTHING)
     begin_mileage = models.CharField(max_length=7)
     end_mileage = models.CharField(max_length=7)
     end_gas_percent = models.CharField(max_length=4)
@@ -278,21 +304,23 @@ class TripDetails(models.Model):
     problem_description = models.TextField()
     reg_date = models.DateTimeField()
     mile_charges = models.FloatField()
-    user = models.ForeignKey(User, on_delete=models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey('AuthUser', models.DO_NOTHING, blank=True, null=True)
 
     class Meta:
+        managed = False
         db_table = 'transport_trip_details'
 
 
 class DriverComments(models.Model):
     id = models.BigAutoField(primary_key=True)
-    posting_user = models.ForeignKey(Driver, on_delete=models.DO_NOTHING, related_name='posted_comments')
-    about_user = models.ForeignKey(Driver, on_delete=models.DO_NOTHING, related_name='received_comments')
+    posting_user = models.ForeignKey(Driver, models.DO_NOTHING)
+    about_user = models.ForeignKey(Driver, models.DO_NOTHING)
     comments_date = models.DateTimeField()
     comments = models.TextField()
-    trip = models.ForeignKey(TripDetails, on_delete=models.DO_NOTHING, blank=True, null=True)
+    trip = models.ForeignKey(TripDetails, models.DO_NOTHING, blank=True, null=True)
 
     class Meta:
+        managed = False
         db_table = 'transport_driver_comments'
 
 
@@ -300,6 +328,7 @@ class VehicleBrand(models.Model):
     name = models.CharField(max_length=255)
 
     class Meta:
+        managed = False
         db_table = 'transport_vehicle_brand'
 
 
@@ -308,16 +337,18 @@ class VehicleType(models.Model):
     capacity = models.SmallIntegerField()
 
     class Meta:
+        managed = False
         db_table = 'transport_vehicle_type'
 
 
 class VehicleComments(models.Model):
     id = models.BigAutoField(primary_key=True)
-    posting_user = models.ForeignKey(Driver, on_delete=models.DO_NOTHING)
-    vehicle = models.ForeignKey(Vehicles, on_delete=models.DO_NOTHING)
+    posting_user = models.ForeignKey(Driver, models.DO_NOTHING)
+    vehicle = models.ForeignKey(Vehicles, models.DO_NOTHING)
     comment_date = models.BigIntegerField()
     type = models.CharField(max_length=25)
     comments = models.CharField(max_length=300)
 
     class Meta:
+        managed = False
         db_table = 'transport_vehicle_comments'
