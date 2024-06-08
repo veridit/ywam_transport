137b7a42dabfb7b229b9368a2fa6ef922dfbfa4a
Jørgen H. Fjeld <jorgen@veridit.no>
2024-06-07 16:34:58 -1000

    devops: Add live data dump
    To always have the latest (+/- 1 day) data.

--- devops/start-from-scratch-again.sh
+++ devops/start-from-scratch-again.sh
@@ -10,12 +10,18 @@ fi
 WORKSPACE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 pushd "$WORKSPACE"
 
+# Connect to and download a fresh copy of the database
+export $(grep -v '^#' .env | xargs)
+
+# Fetch the data only if older than 1 day
+if [[ ! -f "tmp/transport-dump.sql" || $(find "tmp/transport-dump.sql" -mtime +1) ]]; then
+  ssh ec2-user@transportation.uofnkona.edu mysqldump -u"$OLD_MYSQL_USER" "$OLD_MYSQL_DATABASE" -p"$OLD_MYSQL_PASSWORD" > tmp/transport-dump.sql
+fi
+
 docker compose down --volumes
 docker compose up -d db db-old
 
-export $(grep -v '^#' .env | xargs)
-
-# Retry mechanism for pg_isready, up to 10 times with 1 second intervals
+# Wait for postgresql first while mysql loads the dumped data.
 max_retries=10
 count=0
 until docker-compose exec -t db pg_isready -U "$POSTGRES_USER" -d "$POSTGRES_DB"; do
@@ -28,9 +34,9 @@ until docker-compose exec -t db pg_isready -U "$POSTGRES_USER" -d "$POSTGRES_DB"
   sleep 1
 done
 
-# Retry mechanism for MariaDB, up to 10 times with 1 second intervals
+# TODO: wait for a two digit number of tables.
 count=0
-until docker compose exec -T db-old mysql -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = '$MYSQL_DATABASE';" | grep -q '[1-9]'; do
+until docker compose exec -T db-old mysql -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = '$MYSQL_DATABASE';" | grep -qE '^[1-9][0-9]*$'; do
   count=$((count + 1))
   if [ $count -ge $max_retries ]; then
     echo "MariaDB table check failed after $max_retries attempts."
@@ -42,7 +48,9 @@ done
 sleep 1
 
 mkdir -p tmp
-./devops/generate_load_file.sh && pgloader tmp/transport.load
+
+./devops/generate_load_file.sh
+pgloader tmp/transport.load
 docker compose run web python manage.py migrate
 
 popd
