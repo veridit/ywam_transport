f9b116b91732d308e0153e9e7af0b221e520752c
Jørgen H. Fjeld <jorgen@veridit.no>
2024-06-13 17:05:30 -1000

    admin: Use autocomplete for foreign key selection

--- transport/admin.py
+++ transport/admin.py
@@ -1,3 +1,4 @@
+# transport/admin.py
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
@@ -10,8 +11,8 @@ from .models import (
     DriverComments, VehicleBrand, VehicleType, VehicleComments
 )
 
-
 admin.site.unregister(User)
+
 @admin.register(User)
 class UserAdmin(BaseUserAdmin, ModelAdmin):
     pass
@@ -21,6 +22,7 @@ class AbandonTripsAdmin(ModelAdmin):
     list_display = ('id', 'abandon_date', 'reservation', 'user', 'mile_charges', 'calculate_fine', 'miles')
     search_fields = ('notes', 'reservation__id', 'user__username')
     list_filter = ('calculate_fine', 'miles')
+    autocomplete_fields = ['reservation', 'user']
 
 @admin.register(GlobalSettings)
 class GlobalSettingsAdmin(ModelAdmin):
@@ -37,18 +39,21 @@ class RestrictedChargesAdmin(ModelAdmin):
     list_display = ('id', 'vehicle', 'charge_month', 'charge_year', 'department', 'total_charge')
     search_fields = ('vehicle__vehicle_no', 'department__name')
     list_filter = ('charge_month', 'charge_year', 'department')
+    autocomplete_fields = ['vehicle', 'department']
 
 @admin.register(Vehicles)
 class VehiclesAdmin(ModelAdmin):
     list_display = ('id', 'user', 'vehicle_no', 'vin_no', 'make', 'model', 'manufacture_year', 'active', 'restricted', 'sold')
     search_fields = ('vehicle_no', 'vin_no', 'license_plate_no', 'make__name', 'model__type')
     list_filter = ('make', 'model', 'active', 'restricted', 'sold')
+    autocomplete_fields = ['user', 'make', 'model']
 
 @admin.register(InfoLinksPosition)
 class InfoLinksPositionAdmin(ModelAdmin):
     list_display = ('id', 'link', 'position', 'driver_login')
     search_fields = ('link__title',)
     list_filter = ('driver_login',)
+    autocomplete_fields = ['link']
 
 @admin.register(TempMassEmails)
 class TempMassEmailsAdmin(ModelAdmin):
@@ -60,6 +65,7 @@ class DriverAdmin(ModelAdmin):
     list_display = ('id', 'user', 'department', 'phone', 'license_no', 'user_type', 'permit_type', 'new_user', 'driver_permission')
     search_fields = ('user__username', 'department__name', 'phone', 'license_no')
     list_filter = ('user_type', 'permit_type', 'new_user', 'driver_permission')
+    autocomplete_fields = ['user', 'department']
 
 @admin.register(Departments)
 class DepartmentsAdmin(ModelAdmin):
@@ -72,30 +78,35 @@ class VehicleLimitAdmin(ModelAdmin):
     list_display = ('id', 'option', 'department', 'user', 'from_date', 'to_date', 'limit_value')
     search_fields = ('department__name', 'user__username')
     list_filter = ('department',)
+    autocomplete_fields = ['department', 'user']
 
 @admin.register(Reservations)
 class ReservationsAdmin(ModelAdmin):
     list_display = ('id', 'vehicle', 'user', 'planned_passenger_no', 'coordinator_approval', 'planned_departure_datetime', 'planned_return_datetime', 'overnight', 'destination', 'reservation_cancelled')
     search_fields = ('vehicle__vehicle_no', 'user__username', 'destination')
     list_filter = ('coordinator_approval', 'overnight', 'reservation_cancelled', 'repeating', 'no_cost')
+    autocomplete_fields = ['vehicle', 'user']
 
 @admin.register(CommentLog)
 class CommentLogAdmin(ModelAdmin):
     list_display = ('id', 'user', 'comment_time')
     search_fields = ('user__username', 'comments')
     list_filter = ('comment_time',)
+    autocomplete_fields = ['user']
 
 @admin.register(Log)
 class LogAdmin(ModelAdmin):
     list_display = ('id', 'user', 'login_datetime', 'logout_datetime', 'ip_address')
     search_fields = ('user__username', 'ip_address')
     list_filter = ('login_datetime', 'logout_datetime')
+    autocomplete_fields = ['user']
 
 @admin.register(ShopTasks)
 class ShopTasksAdmin(ModelAdmin):
     list_display = ('id', 'user', 'vehicle', 'mileage_reading', 'last_mileage', 'work_type', 'work_start_date', 'task_complete', 'reg_date')
     search_fields = ('user__username', 'vehicle__vehicle_no', 'work_type__type')
     list_filter = ('work_type', 'task_complete', 'reg_date')
+    autocomplete_fields = ['user', 'vehicle', 'work_type']
 
 @admin.register(WorkType)
 class WorkTypeAdmin(ModelAdmin):
@@ -107,29 +118,34 @@ class SpecialNoticeAdmin(ModelAdmin):
     list_display = ('id', 'notice_date', 'user', 'title')
     search_fields = ('user__username', 'title', 'notice')
     list_filter = ('notice_date',)
+    autocomplete_fields = ['user']
 
 @admin.register(ServiceReservations)
 class ServiceReservationsAdmin(ModelAdmin):
     list_display = ('id', 'reg_date', 'vehicle', 'from_datetime', 'to_datetime', 'is_cancelled', 'service_type')
     search_fields = ('vehicle__vehicle_no', 'service_type')
     list_filter = ('is_cancelled', 'service_type')
+    autocomplete_fields = ['vehicle']
 
 @admin.register(ServiceReservationsDetails)
 class ServiceReservationsDetailsAdmin(ModelAdmin):
     list_display = ('id', 'service_reservation', 'reservation')
     search_fields = ('service_reservation__id', 'reservation__id')
+    autocomplete_fields = ['service_reservation', 'reservation']
 
 @admin.register(TripDetails)
 class TripDetailsAdmin(ModelAdmin):
     list_display = ('id', 'reservation', 'begin_mileage', 'end_mileage', 'problem', 'reg_date')
     search_fields = ('reservation__id', 'user__username', 'problem_description')
     list_filter = ('problem', 'reg_date')
+    autocomplete_fields = ['reservation', 'user']
 
 @admin.register(DriverComments)
 class DriverCommentsAdmin(ModelAdmin):
     list_display = ('id', 'posting_user', 'about_user', 'comments_date', 'trip')
     search_fields = ('posting_user__user__username', 'about_user__user__username', 'comments')
     list_filter = ('comments_date',)
+    autocomplete_fields = ['posting_user', 'about_user', 'trip']
 
 @admin.register(VehicleBrand)
 class VehicleBrandAdmin(ModelAdmin):
@@ -146,3 +162,4 @@ class VehicleCommentsAdmin(ModelAdmin):
     list_display = ('id', 'posting_user', 'vehicle', 'comment_date', 'type')
     search_fields = ('posting_user__user__username', 'vehicle__vehicle_no', 'comments')
     list_filter = ('comment_date', 'type')
+    autocomplete_fields = ['posting_user', 'vehicle']
