5f42c0525b98ef4a11a753a24600fa0ef6165aba
Jørgen H. Fjeld <jorgen@veridit.no>
2025-01-25 00:55:03 +0100

    dev: Make start from scratch work again
    By running just screen with bash, allowing the script
    to drive the commands.

--- devops/start-from-scratch-again.sh
+++ devops/start-from-scratch-again.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 #
 
-set -e # Exit on any failure for any command
+# Exit on any failure for any command and on usage of an undefined variable.
+set -euo pipefail
 
-if test -n "$DEBUG"; then
+if test -n "${DEBUG:-}"; then
   set -x # Print all commands before running them - for easy debugging.
 fi
 
 WORKSPACE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 pushd "$WORKSPACE"
 
-# Connect to and download a fresh copy of the database
+# Load .env secrets
 export $(grep -v '^#' .env | xargs)
 
 # Fetch the data only if older than 1 day
@@ -18,16 +19,18 @@ if [[ ! -f "tmp/transport-dump.sql" || $(find "tmp/transport-dump.sql" -mtime +1
   echo "Downloading latest mysql data"
   ssh ec2-user@transportation.uofnkona.edu mysqldump -u"$OLD_MYSQL_USER" "$OLD_MYSQL_DATABASE" -p"$OLD_MYSQL_PASSWORD" > tmp/transport-dump.sql
 else
-  echo "Using exising mysql data"
+  echo "Using existing mysql data"
 fi
 
+echo "Removing volumes for clean start"
 docker compose down --volumes
+echo "Starting database services in the background"
 docker compose up -d db db-old
 
-# Wait for postgresql first while mysql loads the dumped data.
+# Wait for PostgreSQL first while MySQL loads the dumped data.
 max_retries=10
 count=0
-until docker-compose exec -t db pg_isready -U "$POSTGRES_USER" -d "$POSTGRES_DB"; do
+until docker compose exec -T db pg_isready -U "$POSTGRES_USER" -d "$POSTGRES_DB"; do
   count=$((count + 1))
   if [ $count -ge $max_retries ]; then
     echo "pg_isready failed after $max_retries attempts."
@@ -37,9 +40,9 @@ until docker-compose exec -t db pg_isready -U "$POSTGRES_USER" -d "$POSTGRES_DB"
   sleep 1
 done
 
-# TODO: wait for a two digit number of tables.
+# Wait until MySQL has at least a two-digit number of tables
 count=0
-until docker compose exec -T db-old mysql -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = '$MYSQL_DATABASE';" | grep -qE '^[1-9][0-9]*$'; do
+until docker compose exec -T db-old mysql -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = '$MYSQL_DATABASE';" | grep -qE '^[1-9][0-9]$'; do
   count=$((count + 1))
   if [ $count -ge $max_retries ]; then
     echo "MariaDB table check failed after $max_retries attempts."
@@ -53,11 +56,28 @@ sleep 1
 mkdir -p tmp
 
 ./devops/generate_load_file.sh
+echo "Migrate from MySQL to PostgreSQL"
 pgloader tmp/transport.load
-docker compose run web python manage.py migrate
 
-# Creates the super user with the variables from the .env file previously loaed.
-docker-compose run -e DJANGO_SUPERUSER_USERNAME -e DJANGO_SUPERUSER_EMAIL -e DJANGO_SUPERUSER_PASSWORD web python manage.py createsuperuser --noinput
+echo "Dump the structure as it was after the load into PostgreSQL, for later diffing of all the changes."
+./devops/view-schema.sh > tmp/schema-start.txt
 
+echo "Starting web container in the background."
+docker compose up -d web --build
+
+echo "Running Django system check"
+time docker compose exec web python manage.py check
+
+echo "Running migrations"
+docker compose exec web python manage.py migrate --no-input
+
+echo "Repeat the dump after the migrations for diffing."
+./devops/view-schema.sh > tmp/schema-stop.txt
+
+echo "Creates the superuser with the variables from the .env file previously loaded."
+docker compose run -e DJANGO_SUPERUSER_USERNAME -e DJANGO_SUPERUSER_EMAIL -e DJANGO_SUPERUSER_PASSWORD web python manage.py createsuperuser --noinput
+
+echo "Run the server in the background and take over as the dominant process"
+docker compose exec web screen -S django -X stuff 'python manage.py runserver 0.0.0.0:8000\n'
 
 popd
